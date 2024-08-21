6cc6190c6bfebebac7a4ed9cc4f94fc9e8f9e39c
rimskayama <rimskayama@outlook.com>
2024-08-21 23:09:09 +0300

    added ClassArraysFindMax

---
+++ src/main/java/javabasics/ClassArraysFindMax.java
@@ -0,0 +1,44 @@
+package src.main.java.javabasics;
+import java.util.Scanner;
+import java.util.Random;
+
+public class ClassArraysFindMax {
+    public static class Main {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int[] arr = new int[scan.nextInt()];
+            long seed = scan.nextLong();
+            init(arr,seed);
+            print(arr);
+            int ind = findMax(arr);
+            System.out.println(ind);
+        }
+
+        public static void init (int[] arr, long seed) {
+            Random rand = new Random(seed);
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = rand.nextInt(-3, 6);
+            }
+        }
+
+        public static void print (int[] arr) {
+
+            for (int el:arr) {
+                System.out.print(el + " ");
+            }
+            System.out.println();
+        }
+
+        public static int findMax (int[] arr) {
+            int max = arr[0];
+            int maxind = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] > max) {
+                    max = arr[i];
+                    maxind = i;
+                }
+            }
+            return maxind;
+        }
+    }
+}
